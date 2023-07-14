103f1b32df00e6e6782f5617f15f51371d641a8e
d695687 <giuseppe.desantis@burda-forward.it>
2023-07-14 17:47:47 +0200

    clickAndScreenshot function

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ Utilizzando questa semplice applicazione ci si può concentrare completamente su
 1. puntare il mouse sulla progress bar che indica l'avanzamento del video;
 3. attendere 3 secondi e puntare il pulsante che permette di proseguire con il video successivo.
 
+## Issue
+Instead of current page I got a image only containing my desktop's background:
+
+The problem was MacOS permission. The program needs permission to record the screen. 
+I simply added the access to IntelliJ in System Preferences -> Security and Privacy -> Privacy -> Screen Recording and it worked.
+
+do the same for Accessibility
+
 ### TODO
 * Sostituire il wait con l'attesa di un click per ottenere la posizione dei due elementi.
 * Creare un' interfaccia che permetta di mettere in pausa e di riprendere l'esecuzione.
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
-import java.awt.AWTException;
-import java.awt.Color;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.Robot;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    private static int xButton;
-    private static int yButton;
+  private static int xButton;
+  private static int yButton;
 
-    private static int xProgressBar;
-    private static int yProgressBar;
-    private static Color progressBarColor;
+  private static int xA;
+  private static int yA;
+
+  private static int xB;
+  private static int yB;
+
+  private static int screenshotRectangleWidth;
+  private static int screenshotRectangleHeight;
+
+  private static int index = 0;
+  private static int pages = 230;
+  private static int xProgressBar;
+  private static int yProgressBar;
+  private static Color progressBarColor;
 
 
-    public static void main(String[] args) throws AWTException, InterruptedException {
+  public static void main(String[] args) throws AWTException, InterruptedException, IOException {
 
-        Robot robot = new Robot();
+    String currentDir = new File(".").getAbsolutePath();
+    System.out.println ("Current directory: " + currentDir);
 
+    Robot robot = new Robot();
+
+/*
         System.out.println("Punta la progress bar");
         Thread.sleep(2000);
         System.out.println("3");
@@ -43,59 +58,124 @@ public class Main {
         xProgressBar--;
 
         System.out.println("Post Shift: " + xProgressBar + " - "  + yProgressBar);
+*/
 
-        System.out.println("Punta il pulsante");
-        Thread.sleep(2000);
-        System.out.println("3");
-        Thread.sleep(1000);
-        System.out.println("2");
-        Thread.sleep(1000);
-        System.out.println("1");
-        Thread.sleep(1000);
+    System.out.println("Punta il pulsante");
+    Thread.sleep(2000);
+    System.out.println("3");
+    Thread.sleep(1000);
+    System.out.println("2");
+    Thread.sleep(1000);
+    System.out.println("1");
+    Thread.sleep(1000);
 
-        Point buttonPosition = MouseInfo.getPointerInfo().getLocation();
-        xButton = (int) buttonPosition.getX();
-        yButton = (int) buttonPosition.getY();
+    Point buttonPosition = MouseInfo.getPointerInfo().getLocation();
+    xButton = (int) buttonPosition.getX();
+    yButton = (int) buttonPosition.getY();
 
-        System.out.println(xButton + " - "  + yButton);
+    System.out.println(xButton + " - "  + yButton);
 
-        System.out.println("Buon divertimento!");
+    System.out.println("rettangolo punto A");
+    Thread.sleep(2000);
+    System.out.println("3");
+    Thread.sleep(1000);
+    System.out.println("2");
+    Thread.sleep(1000);
+    System.out.println("1");
+    Thread.sleep(1000);
 
-        moveAndClick(robot);
+    Point APosition = MouseInfo.getPointerInfo().getLocation();
+    xA = (int) APosition.getX();
+    yA = (int) APosition.getY();
 
+    System.out.println(xA + " - "  + yA);
+
+    System.out.println("rettangolo punto B");
+    Thread.sleep(2000);
+    System.out.println("3");
+    Thread.sleep(1000);
+    System.out.println("2");
+    Thread.sleep(1000);
+    System.out.println("1");
+    Thread.sleep(1000);
+
+    Point BPosition = MouseInfo.getPointerInfo().getLocation();
+    xB = (int) BPosition.getX();
+    yB = (int) BPosition.getY();
+
+    System.out.println(xB + " - "  + yB);
+
+    screenshotRectangleWidth = xB - xA ;
+    screenshotRectangleHeight = yB - yA ;
+
+    System.out.println(screenshotRectangleWidth + " x "  + screenshotRectangleHeight);
+
+
+    System.out.println("Buon divertimento!");
+
+    while (index < pages) {
+      clickAndScreenshot(robot, index);
+      index++;
     }
 
-    private static void moveAndClick(Robot robot) throws InterruptedException {
+  }
 
-        //eseguo solo se il colore della progress bar non è uguale al colore iniziale
-        if (!robot.getPixelColor(xProgressBar, yProgressBar).equals(progressBarColor)) {
+  private static void moveAndClick(Robot robot) throws InterruptedException {
 
-            //aspetto un altro mezzo secondo, per sicurezza.
-            Thread.sleep(500);
+    //eseguo solo se il colore della progress bar non è uguale al colore iniziale
+    if (!robot.getPixelColor(xProgressBar, yProgressBar).equals(progressBarColor)) {
 
-            //posizione attuale del mouse
-            Point originalPointer = MouseInfo.getPointerInfo().getLocation();
+      //aspetto un altro mezzo secondo, per sicurezza.
+      Thread.sleep(500);
 
-            //sposto il mouse e clicco sul punto desiderato
-            robot.mouseMove(xButton,yButton);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+      //posizione attuale del mouse
+      Point originalPointer = MouseInfo.getPointerInfo().getLocation();
 
-            //riporto il mouse alla posizione iniziale
-            robot.mouseMove((int)originalPointer.getX(),(int)originalPointer.getY());
+      //sposto il mouse e clicco sul punto desiderato
+      robot.mouseMove(xButton,yButton);
+      robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+      robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-            //premo invio nel caso in cui venga visualizzato l'alert: "Video non terminato!"
-            Thread.sleep(500);
+      //riporto il mouse alla posizione iniziale
+      robot.mouseMove((int)originalPointer.getX(),(int)originalPointer.getY());
 
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-        }
+      //premo invio nel caso in cui venga visualizzato l'alert: "Video non terminato!"
+      Thread.sleep(500);
 
-        //aspetto X millisecondi e ripeto l'operazione
-        Thread.sleep(5000); //millisecondi
-
-        //chiamata ricorsiva
-        moveAndClick(robot);
-
+      robot.keyPress(KeyEvent.VK_ENTER);
+      robot.keyRelease(KeyEvent.VK_ENTER);
     }
+
+    //aspetto X millisecondi e ripeto l'operazione
+    Thread.sleep(5000); //millisecondi
+
+    //chiamata ricorsiva
+    moveAndClick(robot);
+
+  }
+
+  private static void clickAndScreenshot(Robot robot, int index) throws InterruptedException, IOException {
+
+    //wait and screenshot
+    Thread.sleep(1000);
+
+    BufferedImage screenShot = robot.createScreenCapture(new Rectangle(xA, yA, screenshotRectangleWidth, screenshotRectangleHeight));
+    ImageIO.write(screenShot, "JPG", new File("./doc/" + index + ".jpg"));
+
+    //wait and click
+    Thread.sleep(1000);
+
+    //posizione attuale del mouse
+    Point originalPointer = MouseInfo.getPointerInfo().getLocation();
+
+    //sposto il mouse e clicco sul punto desiderato
+    robot.mouseMove(xButton,yButton);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+    //riporto il mouse alla posizione iniziale
+    robot.mouseMove((int)originalPointer.getX(),(int)originalPointer.getY());
+
+  }
+
 }
